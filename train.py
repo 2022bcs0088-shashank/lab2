7f06f7eca68d09fe76919b8d02ff0647943b7bac
Shashank Upadhyay <shashank_@outlook.in>
2026-01-09 23:07:18 +0530

    EXP-02: Ridge Regression, Alpha=1.0, Standardization enabled

--- train.py
+++ train.py
@@ -2,8 +2,10 @@ import pandas as pd
 import json
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import os
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
@@ -14,7 +16,10 @@ y = df['quality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. (Edit these lines for Task 5 experiments)
-model = LinearRegression()
+model = Pipeline([
+    ("scaler", StandardScaler()),
+    ("regressor", Ridge(alpha=1.0)),
+])
 model.fit(X_train, y_train)
 
 preds = model.predict(X_test)
