0fecf84a8c8169d109e293b4438d789c491fa044
Shashank Upadhyay <shashank_@outlook.in>
2026-01-09 23:14:42 +0530

    EXP-04: Random Forest, 100 trees, depth=15

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ y = df['quality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. (Edit these lines for Task 5 experiments)
-model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
+model = RandomForestRegressor(n_estimators=100, max_depth=15, random_state=42)
 model.fit(X_train, y_train)
 
 preds = model.predict(X_test)
