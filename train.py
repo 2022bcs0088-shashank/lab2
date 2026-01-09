8ece64845eb6c8b281ae5b758a06e6653000ce89
Shashank Upadhyay <shashank_@outlook.in>
2026-01-09 23:17:41 +0530

    EXP-05: Random Forest, Top 5 Features only

--- train.py
+++ train.py
@@ -9,7 +9,9 @@ import os
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
 df = pd.read_csv(url, sep=';')
 
-X = df.drop('quality', axis=1)
+correlations = df.corr(numeric_only=True)['quality'].drop('quality').abs().sort_values(ascending=False)
+top_features = correlations.head(5).index.tolist()
+X = df[top_features]
 y = df['quality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
