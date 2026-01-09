6d8bd4234afbe7e0b0d5438a9535e5981d805ad8
Shashank Upadhyay <shashank_@outlook.in>
2026-01-09 22:37:19 +0530

    EXP-01: Baseline Linear Regression, All Features, 80/20 split

---
+++ train.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import os
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+df = pd.read_csv(url, sep=';')
+
+X = df.drop('quality', axis=1)
+y = df['quality']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 3. (Edit these lines for Task 5 experiments)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+preds = model.predict(X_test)
+mse = mean_squared_error(y_test, preds)
+r2 = r2_score(y_test, preds)
+
+os.makedirs('output', exist_ok=True)
+joblib.dump(model, 'output/model.pkl')
+
+metrics = {"mse": mse, "r2": r2}
+with open('output/results.json', 'w') as f:
+    json.dump(metrics, f)
+
+print(f"MSE: {mse}")
+print(f"R2 Score: {r2}")
